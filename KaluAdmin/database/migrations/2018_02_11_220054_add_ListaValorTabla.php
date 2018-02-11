be8ee6bbdb8cde5bba3651a7b6bc0273d3f31de0
manuelyanez27 <ing.manuel.yanez@gmail.com>
2018-02-11 17:29:34 -0500

    add Tables Movimientos and lista Valor with constraints

---
+++ KaluAdmin/database/migrations/2018_02_11_220054_add_ListaValorTabla.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddListaValorTabla extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lista_valor', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('categoria');
+            $table->string('valor');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ KaluAdmin/database/migrations/2018_02_11_221449_add_Constraints_ListaValorTabla_y_TablaMovimientos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConstraintsListaValorTablaYTablaMovimientos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('lista_valor') && Schema::hasTable('movimientos') ) {
+            Schema::table('movimientos', function(Blueprint $table){
+             $table->integer('tipo_movimiento_id')->unsigned()->change();   
+             $table->integer('tipo_transaccion_id')->unsigned()->change();   
+             //$table->integer('detalle_movimiento_id')->unsigned()->change();   
+             $table->foreign('tipo_movimiento_id')->references('id')->on('lista_valor');
+             $table->foreign('tipo_transaccion_id')->references('id')->on('lista_valor');
+             //$table->foreign('detalle_movimiento_id')->references('id')->on('lista_valor');
+             
+            }   );
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
