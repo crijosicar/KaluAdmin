27d88fe55134d92be88e7f22ba4b437d94088f76
manuelyanez27 <ing.manuel.yanez@gmail.com>
2018-02-11 17:30:20 -0500

    fixed add table movimientos and lista_valor

--- KaluAdmin/database/migrations/2018_02_11_215642_add_MovimientosTabla.php
+++ KaluAdmin/database/migrations/2018_02_11_215642_add_MovimientosTabla.php
@@ -13,6 +13,16 @@ class AddMovimientosTabla extends Migration
     public function up()
     {
         //
+        Schema::create('movimientos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tipo_movimiento_id');
+            $table->integer('tipo_transaccion_id');
+            $table->bigInteger('monto');
+            $table->integer('detalle_movimiento_id');
+            $table->date('fecha_movimiento');
+            $table->softDeletes();
+            $table->timestamps();
+        });
     }
 
     /**
